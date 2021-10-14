90cd1a4e8d0fb276f1689f50c915dbf13ff9ca35
oblaufuss <ocblaufuss@wpi.edu>
2021-10-14 15:52:51 -0400

    fetch lyrics files

--- public/script.js
+++ public/script.js
@@ -29,9 +29,18 @@ window.onload = function() {
       lyricData[i][1] = split[0];
       lyricData[i][2] = split[1].replace(/-/g, " ");
       lyricData[i][3] = split[2].slice(0, -6).replace(/-/g, " ");
-      //progress.innerHTML = lyricData[i][3];
+      progress.innerHTML = lyricData[i][3];
     }
-   
+
+    progress.innerHTML = "fetching lyrics..."
+    for (let song of lyricData){
+      fetch(song[0])
+      .then(response => response.text())
+      .then(textString => {
+        song[0] = textString;
+      });
+    }
+    progress.innerHTML = "Ready!"
     
   });
   
