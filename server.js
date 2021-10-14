d08ee03497305c04b4fd6cdc61ddf88bc0ffbd7e
Glitch (a4-lillian-garfinkel-) <none>
2021-10-14 00:12:24 +0000

    🌯🎐 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const express    = require('express'),
+      app        = express(),
+      bodyparser = require( 'body-parser' ),
+      dreams     = []
+
+// automatically deliver all files in the public folder
+// with the correct headers / MIME type.
+app.use( express.static( 'public' ) )
+
+// get json when appropriate
+app.use( bodyparser.json() )
+
+// even with our static file handler, we still
+// need to explicitly handle the domain name alone...
+app.get('/', function(request, response) {
+  response.sendFile( __dirname + '/public/index.html' )
+})
+
+app.post( '/submit', bodyparser.json(), function( request, response ) {
+  dreams.push( request.body.newdream )
+  response.writeHead( 200, { 'Content-Type': 'application/json'})
+  response.end( JSON.stringify( dreams ) )
+})
+
+app.listen( process.env.PORT )
+
+
+
+
+
+const path = require("path");
+
