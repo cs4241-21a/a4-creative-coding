ea84ee0ff51fbc8d1029e86cb402a4283413f2f0
Glitch (a4-owen-blaufuss) <none>
2021-10-14 19:37:06 +0000

    request directory from server💈😀 Updated with Glitch

---
+++ public/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var lyricData = [{},{}];
+
+const submit = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+  console.log("Submit");
+  return false;
+};
+
+window.onload = function() {
+
+
+  const progress = document.getElementById("progress");
+
+  progress.innerHTML = "fetching albums...";
+  
+  fetch("/lyrics")
+    .then(response => response.json())
+    .then( data =>{
+    progress.innerHTML = "fetching songs...";
+    let lyricData = [];
+    for (let i = 0; i < data.length; i++) {
+      let split = data[i].split("_");
+      lyricData[i] = {};
+      //filename, year, 
+      lyricData[i][0] = data[i];
+      lyricData[i][1] = split[0];
+      lyricData[i][2] = split[1].replace(/-/g, " ");
+      lyricData[i][3] = split[2].slice(0, -6).replace(/-/g, " ");
+      //progress.innerHTML = lyricData[i][3];
+    }
+   
+    
+  });
+  
+    //enable submit button
+  const button = document.querySelector("button");
+  button.onclick = submit;
+};
