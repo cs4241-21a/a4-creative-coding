f19b618f77ddd6715ed3ee9647461e526f57008c
oblaufuss <ocblaufuss@wpi.edu>
2021-10-15 12:24:54 -0400

    data processing & setup

--- README.markdown
+++ README.markdown
@@ -1,3 +1,16 @@
 Assignment 4 - Creative Coding
 ===
 http://a4-owen-blaufuss.glitch.me
+
+
+
+Requirements
+------------
+- Server, created using Express
+- [D3.js](https://d3js.org)
+- User interface
+
+Special thanks:
+-----------
+- [TMBW](https://tmbw.net), the They Might Be Giants wiki
+- [TMBotG's web scraper](https://github.com/bgporter/tmbotg), for collecting the lyrics 
--- public/script.js
+++ public/script.js
@@ -1,50 +1,82 @@
 "use strict";
 
-var lyricData = [{},{}];
+//data that will be filled in on command
+var lyricData = [{}, {}];
 
-const submit = function(e) {
+var xYears = 2020;
+var xAlbums = [];
+
+const submit = function (e) {
   // prevent default form action from being carried out
   e.preventDefault();
   console.log("Submit");
   return false;
 };
 
-window.onload = function() {
+const showStatus = function (msg) {
+  const progress = document.getElementById("go");
+  progress.value = msg;
+}
 
+window.onload = function () {
 
-  const progress = document.getElementById("progress");
+  showStatus("fetching albums...");
 
-  progress.innerHTML = "fetching albums...";
-  
   fetch("/lyrics")
     .then(response => response.json())
-    .then( data =>{
-    progress.innerHTML = "fetching songs...";
-    let lyricData = [];
-    for (let i = 0; i < data.length; i++) {
-      let split = data[i].split("_");
-      lyricData[i] = {};
-      //filename, year, 
-      lyricData[i][0] = data[i];
-      lyricData[i][1] = split[0];
-      lyricData[i][2] = split[1].replace(/-/g, " ");
-      lyricData[i][3] = split[2].slice(0, -6).replace(/-/g, " ");
-      progress.innerHTML = lyricData[i][3];
-    }
+    .then(data => {
+      showStatus("fetching songs...");
+      let lyricData = [];
+      for (let i = 0; i < data.length; i++) {
+        let split = data[i].split("_");
+        lyricData[i] = {};
+        lyricData[i][0] = data[i]; //filename
+        lyricData[i][1] = split[0]; //year
+        lyricData[i][2] = split[1].replace(/-/g, " "); //album name
+        lyricData[i][3] = split[2].slice(0, -6).replace(/-/g, " "); //song name
+        //showStatus(lyricData[i][3]);
+      }
 
-    progress.innerHTML = "fetching lyrics..."
-    for (let song of lyricData){
-      fetch(song[0])
-      .then(response => response.text())
-      .then(textString => {
-        song[0] = textString;
-      });
-    }
-    progress.innerHTML = "Ready!"
-    
-  });
-  
-    //enable submit button
-  const button = document.querySelector("button");
+      showStatus("fetching lyrics...");
+      for (let song of lyricData) {
+        fetch(song[0])
+          .then(response => response.text())
+          .then(textString => {
+            song[0] = textString;
+
+            //collect album names while we're at it
+            if(!xAlbums.includes(song[2])){
+              xAlbums.push(song[2])
+            }
+          });
+      }
+      showStatus("Search!");
+
+    });
+
+
+
+  //set up bar chart
+  let yearScale = d3.scaleLinear().domain([1980, xYears]).range([0, 400]);
+  let freqScale = d3.scaleLinear().domain([0, 500]).range([0, 400]);
+  let axisLeft = d3.axisLeft(freqScale);
+  let axisBottom = d3.axisBottom(yearScale).tickFormat(d3.format("d"));
+
+  d3.select('#left').call(axisLeft);
+  d3.select('#bottom').call(axisBottom);
+
+  //data is all set up, enable form controls
+  enableForm();
+};
+
+const enableForm = function(){
+  //enable submit button
+  const button = document.getElementById('go');
   button.onclick = submit;
+
+  //enable form elements
+  var inputs = document.getElementsByTagName("input"); 
+    for (var i = 0; i < inputs.length; i++) { 
+        inputs[i].disabled = false;
+    } 
 };
